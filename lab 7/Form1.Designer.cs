aa04d30cd61171eb236620998f79b71780299288
Viktorhjh <Viktorsek7@gmail.com>
2021-12-14 01:57:32 +0100

    Happy New Year

---
+++ lab 7/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace lab_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(93, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(166, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "prezenty pod choinką";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(275, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(150, 21);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "gwiazdę na czubku";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(446, 12);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(75, 21);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "bombki";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(275, 39);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(86, 21);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "łańcuchy";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(93, 39);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(87, 21);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "światełka";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(446, 39);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(124, 21);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.Text = "melodię kolędy";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(550, 550);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(582, 633);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "X-Tree";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ lab 7/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_7
+{
+    public partial class Form1 : Form
+    {
+        bool prezenty = false, bombki = false, gwiazda = false, 
+            swiatelka = false, lancychy = false, melodia = false;
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            clear();
+            Graphics g = panel1.CreateGraphics();
+            int x, y, prezentSize = 40;
+            if (lancychy)
+            {
+                /*g.DrawLine(Pens.Red, 130, 150, 300, 200);
+                g.DrawLine(Pens.Red, 300, 200, 130, 230);
+                g.DrawLine(Pens.Red, 300, 200, 130, 230);
+                g.DrawLine(Pens.Red, 130, 230, 320, 300);
+                g.DrawLine(Pens.Red, 320, 300, 105, 340);
+                g.DrawLine(Pens.Red, 105, 340, 340, 400);*/
+                int i, j;
+                for (i = -5, j = -5; i < 150; i += 3, j++)
+                {
+                    g.FillRectangle(Brushes.Red, 140 + i, 150 + j, 2, 5);
+                }
+
+                for (i = -10, j = 1; i < 170; i += 3, j++)
+                {
+                    g.FillRectangle(Brushes.Red, 140 + i, 230 + j, 2, 6);
+                }
+
+                for (i = 10, j = -15; i < 220; i += 3, j++)
+                {
+                    g.FillRectangle(Brushes.Red, 105 + i, 340 + j, 2, 7);
+                }
+            }           
+
+            if (bombki)
+            {
+                
+                for(int i = 0; i < random.Next(10,25); i++)
+                {
+                    SolidBrush myBr = new SolidBrush(Color.FromArgb(255,
+                    random.Next(150, 255), random.Next(150, 255), random.Next(150, 255)));
+                    g.FillEllipse(myBr, random.Next(100, 300), random.Next(50, 400), 20, 20);
+                }                
+            }
+
+            int alfa = 255, cur;
+            if (swiatelka)
+            {
+                SolidBrush myBr = new SolidBrush(Color.FromArgb(alfa, 255,255,0));      
+                for(int i = 0; i < 500; i++)
+                {
+                    g.FillEllipse(myBr, random.Next(100,300), random.Next(50,400), 5, 5);
+                }                               
+            }
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = @"D:\ProGra2ming\C sharp\lab 7\lab 7\kol.wav";
+            if (melodia)
+            {                
+                player.Play();
+            }
+            else
+            {
+                player.Stop();   
+            }
+            deleteOutOfBounds();
+            if (gwiazda)
+            {
+                x = 175; y = 20;
+                Point points1 = new Point(x + 25, y);
+                Point points2 = new Point(x + 32, y + 20);
+                Point points3 = new Point(x + 50, y + 20);
+                Point points4 = new Point(x + 38, y + 30);
+                Point points5 = new Point(x + 42, y + 50);
+                Point points6 = new Point(x + 25, y + 40);
+                Point points7 = new Point(x + 9, y + 50);
+                Point points8 = new Point(x + 15, y + 30);
+                Point points9 = new Point(x, y + 20);
+                Point points10 = new Point(x + 20, y + 20);
+                Point[] gw = {points1, points2, points3, points4, points5,
+                    points6, points7, points8, points9, points10};
+                g.FillPolygon(Brushes.Yellow, gw);
+                g.DrawPolygon(Pens.Black, gw);
+            }
+            if (prezenty)
+            {
+                x = 245;
+                y = 410;
+                g.FillRectangle(Brushes.Red, x, y, prezentSize, prezentSize);
+                g.FillRectangle(Brushes.Green, x, y + 10, prezentSize, 10);
+                g.FillRectangle(Brushes.Green, x + 10, y, 10, prezentSize);
+
+                x = 290;
+                y = 410;
+                prezentSize += 10;
+                g.FillRectangle(Brushes.Aqua, x, y - 20, prezentSize, prezentSize + 10);
+                g.FillRectangle(Brushes.Yellow, x, y, prezentSize, 10);
+                g.FillRectangle(Brushes.Yellow, x + 20, y - 20, 10, prezentSize + 10);
+            }
+        }
+
+        public void deleteOutOfBounds()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Point points1 = new Point(200, 50);
+            Point points2 = new Point(100, 200);          
+            Point points3 = new Point(0, 0);
+            Point[] triangle1 = { points1, points2, points3};
+            g.FillPolygon(Brushes.White, triangle1);
+
+            Point points4 = new Point(100,200);
+            Point points5 = new Point(150, 200);
+            Point points6 = new Point(80, 300);
+            Point points01 = new Point(0, 0);
+            Point points02 = new Point(0, 300);
+            Point[] triangle2 = { points4, points5, points6, points02, points01 };
+            g.FillPolygon(Brushes.White, triangle2);
+
+            Point points7 = new Point(0, 300);
+            Point points8 = new Point(135, 300);
+            Point points9 = new Point(60, 400);
+            Point[] triangle3 = { points7, points8, points9 };
+            g.FillPolygon(Brushes.White, triangle3);
+
+            Point points10 = new Point(200, 50);
+            Point points11 = new Point(500, 0);
+            Point points12 = new Point(300, 200);
+            Point[] triangle4 = { points10, points11, points12 };
+            g.FillPolygon(Brushes.White, triangle4);
+
+            Point points13 = new Point(200, 50);
+            Point points14 = new Point(500, 0);
+            Point points15 = new Point(300, 200);
+            Point[] triangle5 = { points13, points14, points15 };
+            g.FillPolygon(Brushes.White, triangle5);
+
+            Point points04 = new Point(300, 200);
+            Point points05 = new Point(250, 200);                    
+            Point points07 = new Point(320, 300);
+            Point points008 = new Point(500, 300);
+            Point[] triangle02 = { points04, points05, points07, points008};
+            g.FillPolygon(Brushes.White, triangle02);
+            
+            Point points007 = new Point(265, 300);
+            Point points08 = new Point(340, 300);
+            Point points09 = new Point(340, 400);
+            Point[] triangle03 = { points007, points08, points09 };
+            g.FillPolygon(Brushes.White, triangle03);
+        }
+
+        public void clear()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.FillRectangle(Brushes.White, 0, 0, 550, 550);
+            Point points1 = new Point(200, 50);
+            Point points2 = new Point(100, 200);
+            Point points3 = new Point(300, 200);
+            Point[] triangle1 = { points1, points2, points3 };
+            g.FillPolygon(Brushes.Green, triangle1);
+            points1.Y += 80;
+            points2.Y += 100;
+            points3.Y += 100;
+            points2.X -= 20;
+            points3.X += 20;
+            Point[] triangle2 = { points1, points2, points3 };
+            g.FillPolygon(Brushes.Green, triangle2);
+            points1.Y += 80;
+            points2.Y += 100;
+            points3.Y += 100;
+            points2.X -= 20;
+            points3.X += 20;
+            Point[] triangle3 = { points1, points2, points3 };
+            g.FillPolygon(Brushes.Green, triangle3);
+
+            g.FillRectangle(Brushes.Brown, 175, 400, 50, 50);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            prezenty = !prezenty;
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            gwiazda = !gwiazda;
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            bombki = !bombki;
+        }
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            lancychy = !lancychy;            
+        }
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            swiatelka = !swiatelka;
+        }        
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            melodia = !melodia;
+        }
+    }
+
+}
+/*
+...wiszące na drzewku, a nie w powietrzu (wymaga losowej pozycji),
+światełka...
+...które migają,
+łańcuchy,
+melodię kolędy,
+rysunek nie znika.*/
